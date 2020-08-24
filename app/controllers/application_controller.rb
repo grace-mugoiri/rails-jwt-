6ec02c907ec5960a2806345ab23d256f33831411
grace-mugoiri <gracemugoiri@gmail.com>
2020-08-24 19:03:47 +0300

    add actions for application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+	def not_found
+		render json: { error: 'not_found'}
+	end
+
+	def authorize_request
+		header = request.headers.headers['Authorization']
+		header = header.split(' ').last if header
+		begin
+			@decoded = JsonWebToken.decode(header)
+			@current_user = User.find(@decoded[:user_id])
+		rescue json: { errors: e.message }, status: :message
 end
